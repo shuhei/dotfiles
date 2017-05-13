1c0451bd354c09237d2569e799b17b482e17c9cb
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-05-13 23:12:13 +0200

    chore(vim): update init script to install neovim python3 support

--- brew.sh
+++ brew.sh
@@ -11,3 +11,7 @@ brew install the_silver_searcher
 # Some neovim plugins require python3.
 brew install python3
 brew install neovim/neovim/neovim
+# For python3 support. Otherwise you'll get:
+# [deoplete] deoplete.nvim does not work with this version.
+# [deoplete] It requires Neovim with Python3 support("+python3").
+pip3 install neovim
