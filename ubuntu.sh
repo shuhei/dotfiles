0f66942955fc3c7d163f0417e007e988139e0eea
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-06-20 00:22:14 +0200

    Add a script for setting up Ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt install git python-pip python3-pip neovim
+
+# neovim needs both of python2 and python3.
+# How can I make it work on both? Maybe install directory?
+sudo pip install neovim
+sudo pip3 install neovim
+
+# perf command
+sudo apt install linux-tools-common
