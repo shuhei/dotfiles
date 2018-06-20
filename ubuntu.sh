8da7bb8e442c772e1b7960ba11f9f7373383b523
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-06-20 22:36:50 +0200

    Use diff-highlight on Ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,9 +1,16 @@
-sudo apt install git python-pip python3-pip neovim
+sudo apt update
+sudo apt install git tmux
 
 # neovim needs both of python2 and python3.
 # How can I make it work on both? Maybe install directory?
+sudo apt install python-pip python3-pip neovim
 sudo pip install neovim
 sudo pip3 install neovim
 
 # perf command
 sudo apt install linux-tools-common
+
+# Build git diff-highlight
+pushd /usr/share/doc/git/contrib/diff-highlight
+sudo make
+popd
