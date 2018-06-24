5251b08cdb0e55e72d551cc6ddb5873ea0384ad6
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2018-06-24 20:20:52 +0200

    Use clipboard with tmux on Ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install git tmux docker.io net-tools htop apache2-utils
+sudo apt install git tmux docker.io net-tools htop apache2-utils xclip
 
 # neovim needs both of python2 and python3.
 # How can I make it work on both? Maybe install directory?
