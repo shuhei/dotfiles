ed0be4d47bcc2227432c64698bf7ac3c7b430b81
Shuhei Kagawa <shuhei.kagawa@zalando.de>
2018-07-02 09:10:50 +0200

    Update neovim + python3 config

--- init.sh
+++ init.sh
@@ -27,6 +27,9 @@ mkdir -p ~/.config
 pushd ~/.config
 ln -s ~/dotfiles/.vim nvim
 popd
+# For python3 support. Otherwise you'll get:
+# [deoplete] deoplete.nvim does not work with this version.
+# [deoplete] It requires Neovim with Python3 support("+python3").
 pip3 install --upgrade pip
 pip3 install neovim
 
